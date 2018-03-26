0c225617a5e76939594af76e71a06eb194c2102c
cy-yusuke-abe <y.abe@bottlecube.co.jp>
2018-03-26 10:18:57 +0900

    add sample codes

---
+++ Assets/App/Scenes/GameScene/Scripts/Controllers/HumanAgent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace App.Scenes.GameScene.Controllers
+{
+    public class HumanAgent : MonoBehaviour
+    {
+        public Transform Goal;
+
+        void Start()
+        {
+            var agent = GetComponent<NavMeshAgent>();
+            agent.destination = Goal.position;
+        }
+
+    }
+}
